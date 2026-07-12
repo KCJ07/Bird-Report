67c965c09b138a392957e0ea36fcb22c0538f850
unknown <kjohanson321@gmail.com>
2026-07-12 09:42:36 -0700

    Add local runner project

---
+++ BirdReport.LocalRunner/Program.cs
@@ -0,0 +1,10 @@
+﻿// function to allow this app to run locally
+
+using Amazon.Lambda.TestUtilities;
+
+var function = new Function();
+var context = new TestLambdaContext(); // fake ILambdaContext, just for local testing
+
+await function.FunctionHandler(new object(), context);
+
+Console.WriteLine("Done.");
